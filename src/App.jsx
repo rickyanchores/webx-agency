58546f9e8a62a0e8ae71030af7e8bcc2c0b7ca15
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-23 23:08:32 +0100

    added Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Nav from './Components/Nav/Nav';
+import Footer from './Components/Footer/Footer';
 
 
 const App = () => (
   <div className="App m-5">
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   </div>
 );
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    const navList = [
+        {
+            name: "Home",
+            href: "/"
+        },
+        {
+            name: "About",
+            href: "/about"
+        },
+        {
+            name: "Faq",
+            href: "/faq"
+        }
+    ];
+
+    return (
+        <div className="Nav flex justify-around">
+            <div className="logo">NAV</div>
+            <ul className="navlinks flex">
+                {navList.map((link) => (
+                    <li key={link.name} className="link flex justify-between ml-5">
+                        <Link to={link.href} >{link.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Nav;
