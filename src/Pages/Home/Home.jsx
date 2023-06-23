66d14d0410da0778f03e7c9b62c33dc10e25b3a4
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-06-23 23:14:20 +0100

    edit dev

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Card from '../../Components/Card/Card';
 
 const Home = () => {
   return (
-    <div className="Home w-5/6 m-auto p-5">
+    <div className="Home w-5/6 m-auto mt-5 mb-5 p-5">
         <div className="Title">
             <h1>Webx Agency</h1>
             <p>This is Home</p>
